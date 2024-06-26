d23519263b5f0d39f17f3e58c9fc4968567dcaca
Arun Johnson J <jjohnson.arun@gmail.com>
2024-06-26 18:09:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class MemoryTraining {
                     result += c;
                     newWord = false;
                 } else {
-                    result += ' _ ';
+                    result += '_';
                 }
             } else {
                 result += c;
